182ca5a1728201d60e8c908749ac05e3fed50914
Sergey Beskorovainy <bs261257@gmail.com>
2023-02-27 09:39:37 +0200

    Update

--- scripts/cli/uninstall-windows.service.js
+++ scripts/cli/uninstall-windows.service.js
@@ -6,8 +6,8 @@ const Service = require('node-windows').Service;
 
 // Create a new service object
 const svc = new Service({
-  name: 'Feathers opcua server',
-  description: 'Feathers opcua server application as Windows Service',
+  name: 'Feathers4 opcua server',
+  description: 'Feathers4 opcua server application as Windows Service',
   script: filePath
 });
 
